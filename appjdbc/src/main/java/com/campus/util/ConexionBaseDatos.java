307e5836b5c32e03fb47c80c90ca5f65c27465d1
crodrigr <crodrigr@gmail.com>
2024-01-26 19:25:28 -0500

    conexión a la base datos con getInstance

--- appjdbc/src/main/java/com/campus/util/ConexionBaseDatos.java
+++ appjdbc/src/main/java/com/campus/util/ConexionBaseDatos.java
@@ -1,7 +1,27 @@
 package com.campus.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBaseDatos {
 
-    private static String url="jdbc:mysql://localhost:3308/dabase_factura"
+    private static String url="jdbc:mysql://localhost:3308/dabase_factura";
+    private static String username="root";
+    private static String password="admin";
+    private static Connection conection;
+
+    private ConexionBaseDatos(){
+        
+    }
+
+    public static Connection getInstance()throws SQLException{
+        if(conection==null){
+            conection=DriverManager.getConnection(url,username,password);            
+        }
+        return conection;
+    }
+
+    
     
 }
